8f4c2dd372891fbda754fa70cb154d9c80fe478a
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-01-17 20:56:33 -0500

    The connect wallet styles has been added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithCustomToken, signOut } from 'firebase/auth';
+import Web3 from 'web3';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import "./App.css";
